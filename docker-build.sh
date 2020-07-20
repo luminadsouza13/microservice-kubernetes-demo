79a454dc570203842eb702a69b6eeb6679e56cb9
rj <reeshu13489@yahoo.com>
2020-07-20 01:18:41 -0400

    dockeradd

--- docker-build.sh
+++ docker-build.sh
@@ -17,21 +17,19 @@ GIT_REVISION=`git log -n 1 --pretty=format:'%h'`
 
 VERSION=$(date +%Y%mm%d%H%M%S).git.$GIT_REVISION
 
-echo $VERSION
-exit
 
 docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
+docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:$VERSION
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
+docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:$VERSION
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
+docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:$VERSION
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
+docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:$VERSION
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
